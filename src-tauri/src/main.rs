4dd16a0f9782769de669ae3b9eab6ae5772ec3f3
Martin Betz <martinmichaelbetz@gmail.com>
2024-02-11 15:07:46 +0100

    Add: Append note from textarea

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,39 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use chrono::Local;
+use std::fs::{create_dir_all, OpenOptions};
+use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![save_thought])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn save_thought(thought: String) -> Result<String, String> {
+    let now = Local::now();
+    let date = now.format("%Y-%m-%d").to_string();
+    let timestamp = now.format("%y%m%d%H%M").to_string();
+    let file_name = format!("/Users/martinbetz/Notes/Daily/{}.md", date);
+    let path = PathBuf::from(file_name);
+
+    if let Some(parent) = path.parent() {
+        create_dir_all(parent).expect("Failed to create directories");
+    }
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .expect("Failed to open file");
+
+    if let Err(e) = writeln!(file, "\n**{}**\n{}\n", timestamp, thought) {
+        return Err(format!("Failed to write to file: {}", e));
+    }
+
+    Ok("Thought saved successfully".to_string())
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+const saveThoughtBtn = document.querySelector("#save-thought");
+const thoughtInputEl = document.querySelector("#thought-input");
+const saveMsgEl = document.querySelector("#save-msg");
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+saveThoughtBtn.addEventListener("click", async () => {
+  const thought = thoughtInputEl.value;
+  const response = await invoke("save_thought", { thought });
+  saveMsgEl.textContent = response;
+  thoughtInputEl.value = ""; // Clear the textarea after saving
 });
