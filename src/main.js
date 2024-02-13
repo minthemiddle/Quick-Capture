1bc6f106881be78e21b88980a3787fa54c52e734
Martin Betz <martinmichaelbetz@gmail.com>
2024-02-13 18:13:22 +0100

    Refactor: Log response on console only

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ const { invoke } = window.__TAURI__.tauri;
 const saveThoughtBtn = document.querySelector("#save-thought");
 const thoughtInputEl = document.querySelector("#thought-input");
 const pathInputEl = document.querySelector("#path-input");
-const saveMsgEl = document.querySelector("#save-msg");
 
 saveThoughtBtn.addEventListener("click", async () => {
   const thought = thoughtInputEl.value;
@@ -11,12 +10,11 @@ saveThoughtBtn.addEventListener("click", async () => {
 
   // Check if the path is empty
   if (!path) {
-    saveMsgEl.textContent = "Error: Path cannot be empty";
     return;
   }
 
   const response = await invoke("save_thought", { thought, path });
-  saveMsgEl.textContent = response;
+  console.log(response);
   thoughtInputEl.value = ""; // Clear the textarea after saving
 
   // Save the path to local storage
