67bdfae6b661bfb3cb58e27a270b15a9cdb8074b
Martin Betz <martinmichaelbetz@gmail.com>
2024-02-16 12:29:51 +0100

    Add: Status indicator logic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ const { invoke } = window.__TAURI__.tauri;
 const saveThoughtBtn = document.querySelector("#save-thought");
 const thoughtInputEl = document.querySelector("#thought-input");
 const pathInputEl = document.querySelector("#path-input");
+const statusEl = document.querySelector("#status");
+const statusIconEl = document.querySelector("#status-icon");
+
+const statusUpdates = {
+  success: {
+    message: "Added",
+    iconClass: "text-green-500"
+  },
+  error: {
+    message: "Failed",
+    iconClass: "text-red-500"
+  },
+  edit: {
+    message: "Edit",
+    iconClass: "text-slate-200"
+  },
+  no_path: {
+    message: "No path",
+    iconClass: "text-red-500"
+  }
+};
 
 saveThoughtBtn.addEventListener("click", async () => {
   const thought = thoughtInputEl.value;
@@ -10,16 +31,34 @@ saveThoughtBtn.addEventListener("click", async () => {
 
   // Check if the path is empty
   if (!path) {
+    updateStatus("no_path");
     return;
   }
 
-  const response = await invoke("save_thought", { thought, path });
-  console.log(response);
-  thoughtInputEl.value = ""; // Clear the textarea after saving
-
-  // Save the path to local storage
-  window.localStorage.setItem("path", path);
+  try {
+    const response = await invoke("save_thought", { thought, path });
+    console.log(response);
+    thoughtInputEl.value = ""; // Clear the textarea after saving
+    window.localStorage.setItem("path", path); // Save the path to local storage
+    updateStatus("success");
+  } catch (error) {
+    console.error(error);
+    updateStatus("error"); // Update status to "Failed" on error
+  }
 });
+
+function updateStatus(statusKey) {
+  const { message, iconClass } = statusUpdates[statusKey];
+  statusEl.textContent = message;
+  statusIconEl.className = iconClass;
+
+  // Revert to default status after a delay
+  setTimeout(() => {
+    const defaultStatus = statusUpdates['edit'];
+    statusEl.textContent = defaultStatus.message;
+    statusIconEl.className = defaultStatus.iconClass;
+  }, statusKey === 'edit' ? 0 : 3000); // Immediate revert for 'edit', delay for others
+}
 
 
 thoughtInputEl.addEventListener('keydown', handleKeyDown);
